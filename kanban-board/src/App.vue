c6cab72ec22f11516d1bc48cefb17d462ce5eed4
Nicolai Klokmose <angryniczorn@gmail.com>
2024-10-24 00:48:04 +0200

    e2e2e211

--- kanban-board/src/App.vue
+++ kanban-board/src/App.vue
@@ -47,5 +47,154 @@ export default defineComponent({
     
   },
 })
+</script><template>
+  <v-app class="app">
+    <Header />
+    <v-main>
+      <v-container fluid>
+        <KanbanBoard />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import KanbanBoard from './components/KanbanBoard.vue';
+
+export default {
+  components: {
+    Header,
+    KanbanBoard
+  }
+}
+</script>
+
+<style>
+.app {
+  background-color: #f9f9f9;
+  color: #333;
+  font-family: 'Arial', sans-serif;
+}
+</style>
+
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+dasdasdasdasdasdasdasdsd<template>
+  <v-app class="app">
+    <Header />
+    <v-main>
+      <v-container fluid>
+        <KanbanBoard />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import KanbanBoard from './components/KanbanBoard.vue';
+
+export default {
+  components: {
+    Header,
+    KanbanBoard
+  }
+}
+</script>
+
+<style>
+.app {
+  background-color: #f9f9f9;
+  color: #333;
+  font-family: 'Arial', sans-serif;
+}
+</style>
+
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+dasdasdasdasdasdasdasdsd<template>
+  <v-app class="app">
+    <Header />
+    <v-main>
+      <v-container fluid>
+        <KanbanBoard />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import KanbanBoard from './components/KanbanBoard.vue';
+
+export default {
+  components: {
+    Header,
+    KanbanBoard
+  }
+}
+</script>
+
+<style>
+.app {
+  background-color: #f9f9f9;
+  color: #333;
+  font-family: 'Arial', sans-serif;
+}
+</style>
+
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
 </script>
 dasdasdasdasdasdasdasdsd
